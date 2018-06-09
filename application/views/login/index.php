bc979ab6fa84d07006572ef7b7e478305e89779e
sgonzalezme <s.gonzalezme@alumnos.urjc.es>
2018-06-09 14:14:42 +0200

    Forms cleaned up

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,6 +1,5 @@
 <h3>Login Form</h3><hr/>
-
-<div class='error_msg'>
+<div class="error_msg">
     <?php if (isset($error_message)) {
         echo $error_message;
     }
@@ -8,11 +7,14 @@
     ?>
 </div>
 
-<?php echo form_open('login/index'); ?>
-    <label for="username">Username :</label>
-    <input type="text" name="username" id="username" placeholder="username"/><br /><br />
-    <label for="password">Password :</label>
-    <input type="password" name="password" id="password" placeholder="**********"/><br/><br />
-    <input type="submit" value="Login"/><br />
+<div class="form-group col-md-4">
+    <?php echo form_open('login/index'); ?>
+        <label for="username">Username :</label>
+        <input type="text" name="username" id="username" class="form-control" placeholder="username"/><br />
+        <label for="password">Password :</label>
+        <input type="password" name="password" id="password" class="form-control" placeholder="**********"/><br/>
+        <button class="btn btn-primary" type="submit">Login</button><br />
+    <?php echo form_close(); ?>
+
     <a href="<?php echo site_url('/register/index')?>">Not a member? Sign up here</a>
-<?php echo form_close(); ?>
+</div>
--- application/views/register/index.php
+++ application/views/register/index.php
@@ -1,21 +1,22 @@
 <h3>Registration form</h3><hr/>
 <div class="error_msg">
-    <?php
-        echo validation_errors();
-        if (isset($error_message)) {
-            echo $error_message;
-        }
+    <?php if (isset($error_message)) {
+        echo $error_message;
+    }
+    echo validation_errors();
     ?>
 </div>
-<?php echo form_open('register/index'); ?>
 
-    <label for="username">Username: </label>
-    <input name="username" id="username" type="text" placeholder="john.doe" /> <br/>
-    <label for="password">Password: </label>
-    <input name="password" id="password" type="password" placeholder="********" /> <br/>
-    <label for="repeat_password">Repeat password: </label>
-    <input name="repeat_password" id="repeat_password" type="password" placeholder="********" /> <br/>
-    <input type="submit" value="Create user"/><br />
+<div class="form-group col-md-4">
+    <?php echo form_open('register/index'); ?>
+        <label for="username">Username: </label>
+        <input name="username" id="username" type="text" class="form-control" placeholder="john.doe" /> <br/>
+        <label for="password">Password: </label>
+        <input name="password" id="password" type="password" class="form-control" placeholder="********" /> <br/>
+        <label for="repeat_password">Repeat password: </label>
+        <input name="repeat_password" id="repeat_password" type="password" class="form-control" placeholder="********" /> <br/>
+        <button class="btn btn-primary" type="submit">Create user</button><br />
+    <?php echo form_close(); ?>
 
-<?php echo form_close(); ?>
-<a href="<?php echo site_url('/login/index') ?>">Already an user? Log in</a>
+    <a href="<?php echo site_url('/login/index') ?>">Already an user? Log in</a>
+</div>
